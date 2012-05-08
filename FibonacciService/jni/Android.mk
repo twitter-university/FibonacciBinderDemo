50a10b6731398351555008b7f6d6fb97ed7af9cb
Aleksandar Gargenta <sasa@gargenta.com>
2012-05-07 20:40:52 -0700

    Switched to FibonacciNative version of the libs

--- FibonacciService/jni/Android.mk
+++ FibonacciService/jni/Android.mk
@@ -1,8 +1,25 @@
+# Defines the root to all other relative paths
+# The macro function my-dir, provided by the build system,
+# specifies the path of the current directory (i.e. the 
+# directory containing the Android.mk file itself)
 LOCAL_PATH := $(call my-dir)
 
+# Clear all LOCAL_XXX variables with the exception of 
+# LOCAL_PATH (this is needed because all variables are global)
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := com_marakana_android_fibonaccinative_FibLib.c  # <1>
-LOCAL_MODULE    := com_marakana_android_fibonaccinative_FibLib    # <2>
+# List all of our C/C++ files to be compiled (header file 
+# dependencies are automatically computed)
+LOCAL_SRC_FILES := com_marakana_android_fibonaccinative_FibLib.c
 
+# The name of our shared module (this name will be prepended
+# by lib and postfixed by .so)
+LOCAL_MODULE := com_marakana_android_fibonaccinative_FibLib
+
+# We need to tell the linker about our use of the liblog.so
+LOCAL_LDLIBS += -llog
+
+# Collects all LOCAL_XXX variables since "include $(CLEAR_VARS)"
+#  and determines what to build (in this case a shared library)
 include $(BUILD_SHARED_LIBRARY)
+
--- FibonacciService/jni/com_marakana_android_fibonaccinative_FibLib.c
+++ FibonacciService/jni/com_marakana_android_fibonaccinative_FibLib.c
@@ -1,23 +1,30 @@
-#include "com_marakana_android_fibonaccinative_FibLib.h" /* <1> */
+/* Include the header file that was created via "javah -jni" command */
+#include "com_marakana_android_fibonaccinative_FibLib.h"
+#include <android/log.h>
 
-jlong fib(jlong n) { /* <2> */
+/* Recursive implementation of the fibonacci algorithm (in a helper function) */
+static jlong fib(jlong n) {
     return n <= 0 ? 0 : n == 1 ? 1 : fib(n - 1) + fib(n - 2);
 }
 
+/* Actual implementation of JNI-defined `fibNR` (recursive) function */
 JNIEXPORT jlong JNICALL Java_com_marakana_android_fibonaccinative_FibLib_fibNR
-  (JNIEnv *env, jclass clazz, jlong n) { /* <3> */
-    return fib(n);
+  (JNIEnv *env, jclass clazz, jlong n) {
+	__android_log_print(ANDROID_LOG_DEBUG, "FibLib.c", "fibNR(%lld)", n);
+	return fib(n);
 }
 
+/* Actual implementation of JNI-defined `fibNI` (iterative) function */
 JNIEXPORT jlong JNICALL Java_com_marakana_android_fibonaccinative_FibLib_fibNI
-  (JNIEnv *env, jclass clazz, jlong n) { /* <4> */
-    jlong previous = -1;
-    jlong result = 1;
-    jlong i;
-    for (i = 0; i <= n; i++) {
-        jlong sum = result + previous;
-        previous = result;
-        result = sum;
-    }
-    return result;
+  (JNIEnv *env, jclass clazz, jlong n) {
+	jlong previous = -1;
+	jlong result = 1;
+	jlong i;
+	__android_log_print(ANDROID_LOG_DEBUG, "FibLib.c", "fibNI(%lld)", n);
+	for (i = 0; i <= n; i++) {
+		jlong sum = result + previous;
+		previous = result;
+		result = sum;
+	}
+	return result;
 }
